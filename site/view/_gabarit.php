2d788747bf01bf739db47eee6b1eb942f4961595
Anthony André <anthony-andre@live.fr>
2022-02-01 16:40:21 +0100

    Personnalisation du Gabarit

--- site/view/_gabarit.php
+++ site/view/_gabarit.php
@@ -13,18 +13,17 @@
 
 <div id="site">
 	<header>
-        <img src=" <?= MEDIA; ?>/img/Logo_Les_Magiciens_Du_Fouet.jpg" alt="logo robot patissier">
+        <a href="#"><img src=" <?= MEDIA; ?>/img/Logo_Les_Magiciens_Du_Fouet.jpg" alt="logo robot patissier"></a>
         <h1>Les Magiciens Du Fouet</h1>
     </header>
     <nav>
     	<ul>
-            <li class="titreAside">Nos Recettes :</li>
+            <!-- <li class="titreAside">Nos Recettes :</li>
             <li><a href="#">Le chausson aux pommes</a></li>               
             <li><a href="#">Le croque-monsieur</a></li>               
             <li><a href="#">Les choux à la crème</a></li>               
-            <li><a href="#">La tartiflette</a></li> 
-            <div></div>
-            <li class="titreAside">Nos chefs fouetteurs :</li>
+            <li><a href="#">La tartiflette</a></li>  -->
+            <li><a href="#">Nos chefs fouetteurs :</a></li>
             <li><a href="#">Jean-Pierre Coffre</a></li>             
             <li><a href="#">Maï Thé</a></li>             
             <li><a href="#">Ed Chebest</a></li>             
